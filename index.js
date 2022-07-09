526f08d87beb0d6ca91669148e64796c0e1b503a
vishalvsk <vishalkaralevsk@gmail.com>
2022-07-10 00:26:05 +0530

    jsDay1

--- index.js
+++ index.js
@@ -1,3 +1,65 @@
-console.log("Thapa Technical");
-var head = document.querySelector("#h1");
-head.innerHTML = "LEARNING JS BY THAPA TECHNICAL ";
+// console.log("Thapa Technical");
+// var head = document.querySelector("#h1");
+// head.innerHTML = "LEARNING JS BY THAPA TECHNICAL ";
+
+/*  first interview question - 
+1 what is Nan
+ so Nan is a prperty of the global object.
+ in other words, it  is a varible in global scope.
+ The initial value of Nan is not-a-number
+ */
+// var myPhoneNumber = 9860809706;
+// var myName = "vishal";
+// console.log(myPhoneNumber, myName);
+// console.log(typeof (myPhoneNumber));
+// console.log(typeof (myName));
+
+// Expression and Operartors
+// assignments operator
+// arithmatic operator
+// comparision operator
+// logical operator
+// String operator
+// conditional operator
+// String operator - concatinate two strings with the help of the string operator
+
+//   problems
+// console.log(3 ** 3);
+//   add string in a number
+// console.log(123 + "123");
+
+// write a function which swap two numbers
+// var a = 2;
+// var b = 3;
+// var c = b;
+// var b = a;
+// var a = c;
+// console.log(a);
+// console.log(b);
+
+// write a function which swap two numbers without using third varible
+// var a = 10;
+// var b = 5;
+// var a = a + b;  // a = 10 + 5 = 15;
+// var b = a - b;
+// var a = a - b;
+// console.log(a);
+// console.log(b);
+
+// interview question -
+// what is the differce between the == and  === ;
+
+//  solution
+// var num1 = 5;
+// var num2 = "5";
+
+// console.log(num1 == num2);
+// console.log(num1 === num2);
+
+// triple equal to checks the data types also
+
+
+
+
+
+
