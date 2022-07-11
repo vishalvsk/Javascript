d2cd19304f9ffaa7c6ce0309a701aa3c9fb0c80b
vishalvsk <vishalkaralevsk@gmail.com>
2022-07-11 12:15:33 +0530

    function.js

---
+++ function.js
@@ -0,0 +1,80 @@
+// function defination
+// before we use a function, we need to define the function
+// A function defination syntax
+
+// function f1() {
+//     // statement of tha function
+//     return 1;
+
+// }
+// function f2() {
+//     return 2;
+    
+// }
+// function f3() {
+//     return 3;
+    
+// }
+// function f4() {
+//     return 4;
+    
+// }
+// function f6() {
+//     return 5;
+    
+// }
+
+// var a = 1;
+// var b = 10;
+// var sum2 = a + b;
+// console.log(sum2);
+
+// // // make a function of sum of two numbers
+// function sum(a,b) {
+//     return a + b;
+// }
+// //  function call
+
+// var output = sum(50, 50);
+// console.log(output);
+
+//  what is differnce between function parameters and function argumenrs
+//  function parameters are the names listed in the function's defination
+// function arguments are the real values passed to the function.
+
+//  why functions should we use
+//  you can resue that code, and use it many times
+//  we just give the different arguments
+//  Do not repeat yourself
+
+
+//  function expression
+//  create a fun use in tne varible
+
+// function sum(a, b) {
+//     var total =  a + b;
+//     return total;
+// }
+
+// var funExp = sum(10, 20);
+// funExp;
+
+
+//  here return keyword
+
+// function sum(a, b) {
+//     return a + b;
+// }
+
+// var funExp = sum(10, 20);
+// console.log(funExp);
+
+
+//  anonymous function
+
+// var funExp = function(a, b) {
+//     return a + b;
+// }
+
+
+// console.log(funExp(5,10));
