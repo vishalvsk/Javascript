1970520899c5559b5af52bfa5723cc2d2e376cf8
vishalvsk <vishalkaralevsk@gmail.com>
2022-07-12 19:02:52 +0530

    MathObj.js

---
+++ mathObj.js
@@ -0,0 +1,21 @@
+//      --   Mth Object in js
+
+//  Math.PI
+// console.log(Math.PI);
+
+// // Math.round();
+// let num = 10.245522;
+// console.log(Math.round(num));
+
+
+//  Math.pow();
+
+// let num = 8;
+// console.log(Math.pow(num, 2));
+
+//  Math.sqrt();
+//  Math.abs();
+
+// let num = -1245;
+// console.log(Math.abs(num));
+
